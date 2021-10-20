4ac5ead3c449665f4716c4e93d8c538ecfcf30c0
Faris <farisce.009@gmail.com>
2021-10-21 00:17:40 +0700

    Update updateModelManual.sh

--- updateModelManual.sh
+++ updateModelManual.sh
@@ -4,3 +4,7 @@ wget https://inose.id:5555/api/update/downloadModel?serial_number=1
 mv 'downloadModel?serial_number=1' ~/.config/.enose-model/model.zip
 cd ~/.config/.enose-model/
 unzip model.zip
+wget https://raw.githubusercontent.com/Izatou/inosek/main/index.py
+sudo cp index.py ~/.config/.enose-model/
+
+echo Beress gan
