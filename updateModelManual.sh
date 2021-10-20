07a8a0bac6d94830661b73c0645a1d87e76a7e3a
Faris <farisce.009@gmail.com>
2021-10-21 00:03:49 +0700

    Update updateModelManual.sh

--- updateModelManual.sh
+++ updateModelManual.sh
@@ -1,3 +1,6 @@
+sudo rm -r ~/.config/.enose-model
+mkdir ~/.config/.enose-model
 wget https://inose.id:5555/api/update/downloadModel?serial_number=1
 mv 'downloadModel?serial_number=1' ~/.config/.enose-model/model.zip
-unzip ~/.config/.enose-model/model.zip
+cd ~/.config/.enose-model/
+unzip model.zip
